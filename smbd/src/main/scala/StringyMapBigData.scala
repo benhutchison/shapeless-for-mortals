4e0424e911ec27729ef9c8cc0c6d6478413d4530
Ben Hutchison <brhutchison@gmail.com>
2016-03-14 09:16:27 +1100

    working :)

--- smbd/src/main/scala/StringyMapBigData.scala
+++ smbd/src/main/scala/StringyMapBigData.scala
@@ -6,7 +6,7 @@
  */
 package s4m.smbd
 
-import shapeless._, labelled.{ field, FieldType }
+import shapeless._, shapeless.labelled._
 
 /**
  * This exercise involves writing tests, only a skeleton is provided.
@@ -17,6 +17,8 @@ import shapeless._, labelled.{ field, FieldType }
 package object api {
   type StringyMap = java.util.HashMap[String, AnyRef]
   type BigResult[T] = Either[String, T] // aggregating errors doesn't add much
+
+  def bigResult[T](t: T): BigResult[T] = Right[String, T](t)
 }
 
 package api {
@@ -42,12 +44,109 @@ package object impl {
   import api._
 
   // EXERCISE 1.1 goes here
-  // implicit def hNilBigDataFormat = ???
-  // implicit def hListBigDataFormat = ???
-  // implicit def cNilBigDataFormat = ???
-  // implicit def coproductBigDataFormat = ???
+  implicit def hNilBigDataFormat = new BigDataFormat[HNil] {
+    override def label: String = ???
 
-  implicit def familyBigDataFormat[T] = ???
+    override def toProperties(t: HNil): StringyMap = new java.util.HashMap()
+
+    override def fromProperties(m: StringyMap): BigResult[HNil] = Right(HNil)
+  }
+
+  implicit def hListBigDataFormat[Name <: Symbol, Head, Tail <: HList](
+    implicit
+    key: Witness.Aux[Name],
+    pf: SPrimitive[Head],
+    bft: Lazy[BigDataFormat[Tail]]
+  ): BigDataFormat[FieldType[Name, Head] :: Tail] = new BigDataFormat[FieldType[Name, Head] :: Tail] {
+    override def label: String = "hListBigDataFormat"
+
+    override def toProperties(t: FieldType[Name, Head] :: Tail): StringyMap = {
+      val m = bft.value.toProperties(t.tail)
+      m.put(key.value.name, pf.toValue(t.head))
+      m
+    }
+
+    override def fromProperties(m: StringyMap): BigResult[FieldType[Name, Head] :: Tail] = {
+      val headR = m.get(key.value.name) match {
+        case null =>
+          Left("some sensible error here")
+        case found =>
+          Right(pf.fromValue(found))
+      }
+      val tailR = bft.value.fromProperties(m)
+      for {
+        h <- headR.right
+        t <- tailR.right
+      } yield (field[Name](h) :: t)
+    }
+  }
+
+  implicit object StringSPrimitive extends SPrimitive[String] {
+    def toValue(v: String): AnyRef = v
+    def fromValue(v: AnyRef): String = v.asInstanceOf[String]
+  }
+
+  implicit object IntSPrimitive extends SPrimitive[Int] {
+    def toValue(v: Int): AnyRef = Integer.valueOf(v)
+    def fromValue(v: AnyRef): Int = v.asInstanceOf[Integer].toInt
+  }
+
+  implicit object DoubleSPrimitive extends SPrimitive[Double] {
+    def toValue(v: Double): AnyRef = java.lang.Double.valueOf(v)
+    def fromValue(v: AnyRef): Double = v.asInstanceOf[java.lang.Double].toDouble
+  }
+
+  implicit object BooleanSPrimitive extends SPrimitive[Boolean] {
+    def toValue(v: Boolean): AnyRef = java.lang.Boolean.valueOf(v)
+    def fromValue(v: AnyRef): Boolean = v.asInstanceOf[java.lang.Boolean].booleanValue()
+  }
+
+  implicit def cNilBigDataFormat = new BigDataFormat[CNil] {
+    override def label: String = "CNil"
+
+    override def toProperties(t: CNil): StringyMap = ???
+
+    override def fromProperties(m: StringyMap): BigResult[CNil] = ???
+  }
+  implicit def coproductBigDataFormat[Name <: Symbol, Head, Tail <: Coproduct](
+    implicit
+    key: Witness.Aux[Name],
+    ch: Lazy[BigDataFormat[Head]],
+    ct: Lazy[BigDataFormat[Tail]]
+  ): BigDataFormat[FieldType[Name, Head] :+: Tail] = new BigDataFormat[FieldType[Name, Head] :+: Tail] {
+    def label: String = "coproductBigDataFormat"
+
+    def toProperties(t: FieldType[Name, Head] :+: Tail): StringyMap = t match {
+      case Inl(found) =>
+        val m = ch.value.toProperties(found)
+        m.put("type", key.value.name)
+        m
+      case Inr(tail) => ct.value.toProperties(tail)
+    }
+
+    def fromProperties(m: StringyMap): BigResult[FieldType[Name, Head] :+: Tail] = {
+      if (m.containsKey("type") && m.get("type") == key.value.name)
+        ch.value.fromProperties(m).right.map(v => Inl(field[Name](v)))
+      else
+        ct.value.fromProperties(m).right.map(Inr(_))
+    }
+  }
+
+  implicit def familyBigDataFormat[T, Repr](
+    implicit
+    g: LabelledGeneric.Aux[T, Repr],
+    fr: Lazy[BigDataFormat[Repr]]
+  ) = new BigDataFormat[T] {
+    override def label: String = "foibles"
+
+    override def toProperties(t: T): StringyMap = {
+      fr.value.toProperties(g.to(t))
+    }
+
+    override def fromProperties(m: StringyMap): BigResult[T] = {
+      fr.value.fromProperties(m).right.map(g.from(_))
+    }
+  }
 }
 
 package impl {
